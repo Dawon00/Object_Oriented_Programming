837ccae0aaa18296a194214bcebe053e9da9cf71
Dawon00 <da6921@naver.com>
2021-10-13 04:50:19 +0900

    first commit

---
+++ chap02-ex02/src/CircleArea.java
@@ -0,0 +1,14 @@
+
+public class CircleArea { //원의 면적을 구하는 프로그램
+
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		
+		double radius = 10.0;
+		double circleArea = radius*radius*PI;
+		
+		System.out.println("원의 면적 = " + circleArea);
+
+	}
+
+}

1e2f6922dc4d73d09adde1b5f31d6c5d8de2dd36
Dawon00 <da6921@naver.com>
2021-12-11 20:30:38 +0900

    first commit

---
+++ 별찍기/src/별찍기/삼각형1.java
@@ -0,0 +1,14 @@
+package º°Âï±â;
+
+public class »ï°¢Çü1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<5;i++){
+			for(int j=0;j<i;j++){
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
